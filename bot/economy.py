75ad96b7a855fe2d544e8fc5e4d8565153f0f4cc
bishalkumar000001 <bishalkumar000001@gmail.com>
2026-07-21 11:22:51 +0530

    Update economy.py

--- bot/economy.py
+++ bot/economy.py
@@ -5,13 +5,13 @@ from models import WIN_COINS
 async def award_winner(winner_id: int, chat_id: int = 0):
     await db.update_user_stats(winner_id, won=True, coins_delta=WIN_COINS)
     if chat_id:
-        await db.log_game_result(winner_id, chat_id, won=True, coins_earned=coins_won))
+        await db.log_game_result(winner_id, chat_id, won=True, coins_earned=coins_won)
 
 
 async def record_loss(loser_id: int, chat_id: int = 0):
     await db.update_user_stats(loser_id, won=False, coins_delta=0)
     if chat_id:
-        await db.log_game_result(loser_id, chat_id, won=False, coins_earned=0))
+        await db.log_game_result(loser_id, chat_id, won=False, coins_earned=0)
 
 
 
